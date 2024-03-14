40bd7dc909026510ba558115830ce6fd69a8f192
Roshan shah <roshan9808671438@gamli.com>
2024-03-14 10:29:48 +0545

    NCC Lab

---
+++ Lab-1/qno4/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Students
+{
+    private string[] names = new string[5];
+
+    public string this[int index]
+    {
+        get { return names[index]; }
+        set { names[index] = value; }
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Students students = new Students();
+        students[0] = "Alish";
+        students[1] = "Swikar";
+        students[2] = "Prakar";
+        students[3] = "Prashanna";
+        students[4] = "Saurav";
+
+        Console.WriteLine("Student 2: " + students[1]);
+    }
+}
---
+++ Lab-2/qno6/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[] names = { "Ram", "Shyam", "Hari", "Bikash", "Mahesh" };
+        var sortedNames = names.OrderBy(name => name);
+        foreach (var name in sortedNames)
+        {
+            Console.WriteLine(name);
+        }
+    }
+}
